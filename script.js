d4072b691887eb3288ac97976c51b5a86cbb63b6
Julian Pinzón <101753352+julianpinzon2018@users.noreply.github.com>
2022-10-26 11:54:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const saludoText = document.querySelector("#saludo-text");
 const proyectosSection = document.querySelector("#projects");
 const body = document.querySelector("#body");
 const proyectosTitle = document.querySelector("#proyectos-title");
-const projectTitl = document.querySelector(".project-titl");
+const projectTitl = document.querySelectorAll("#proyectos-caja p");
 const button = document.querySelector("#button");
 const redes = document.querySelector("#redes");
 const redesTItle = document.querySelector("#redes-title");
+const li = document.querySelectorAll("#nav li");
 
 let isDark = true;
-burn.onclick = () => {
+
+burn.addEventListener("click", () => {
   header.classList.toggle("active-header");
   activeSaludoCaja.classList.toggle("active-saludo-caja");
   saludo.classList.toggle("active-saludo");
@@ -20,10 +22,15 @@ burn.onclick = () => {
   proyectosSection.classList.toggle("active-proyectos-section");
   body.classList.toggle("active-body");
   proyectosTitle.classList.toggle("active-proyectos-title");
-  projectTitl.classList.toggle("active-project-titl");
+  projectTitl.forEach((elemento, index, arreglo) => {
+    arreglo[index].classList.toggle("active-project-titl");
+  });
   button.classList.toggle("active-button");
   redes.classList.toggle("active-redes");
   redesTItle.classList.toggle("active-redes-title");
+  li.forEach((elemento, index, arreglo) => {
+    arreglo[index].classList.toggle("active-li");
+  });
 
   isDark = false;
-};
+});
